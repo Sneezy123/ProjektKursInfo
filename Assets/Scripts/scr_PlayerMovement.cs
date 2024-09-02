8bcea44d2560dcccc90feca202db71e0244fb1ff
Nils <nilshendrik13@gmail.com>
2024-09-02 18:16:41 +0200

    fix air friction

--- Assets/Scripts/scr_PlayerMovement.cs
+++ Assets/Scripts/scr_PlayerMovement.cs
@@ -80,7 +80,7 @@ public class PlayerMovementAdvanced : MonoBehaviour
     private void Update()
     {
         // ground check
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.02f, whatIsGround);
        
         MyInput();
         SpeedControl();
