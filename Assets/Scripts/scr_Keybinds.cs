1fa5d7840c1a50cae0e910ca196fe751877e30d7
Jannik <120487272+Jannik440@users.noreply.github.com>
2024-09-03 12:10:37 +0200

    scr_Keybinds.cs

--- Assets/Scripts/scr_Keybinds.cs
+++ Assets/Scripts/scr_Keybinds.cs
@@ -7,17 +7,17 @@ using UnityEngine.UI;
 // Script not in use!!!!
 // Test
 
-public class scr_KeyBinds : MonoBehaviour
+public class scr_Keybinds : MonoBehaviour
 {
     [Header("Movement")]
-	public KeyCode walkForward = KeyCode.W;
-	public KeyCode walkLeft = KeyCode.A;
-	public KeyCode walkBack = KeyCode.S;
-	public KeyCode walkRight = KeyCode.D;
+				public KeyCode walkForward = KeyCode.W;
+				public KeyCode walkLeft = KeyCode.A;
+				public KeyCode walkBack = KeyCode.S;
+				public KeyCode walkRight = KeyCode.D;
 
     public KeyCode sprintKey = KeyCode.LeftShift;
     public KeyCode crouchKey = KeyCode.LeftControl;
 	
     [Header("Interactions")]
-	public KeyCode interact = KeyCode.E;
+				public KeyCode interact = KeyCode.E;
 }
