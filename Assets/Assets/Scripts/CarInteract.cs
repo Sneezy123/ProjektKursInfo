d3a15e35a577b9d3a9146d24e42a7368158623fc
Nils <nilshendrik13@gmail.com>
2024-09-10 12:42:24 +0200

    add car interaction system

--- Assets/Assets/Scripts/CarInteract.cs
+++ Assets/Assets/Scripts/CarInteract.cs
@@ -2,17 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarInteract : MonoBehaviour
+public class CarInteract : MonoBehaviour, IInteractable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private Animator doorAnim;
+    private bool isCarDoorOpen = false;
+    private bool isTrunkDoorOpen = false;
+
+    private void Awake(){
+    doorAnim = gameObject.GetComponent<Animator>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+   public void Interact()
     {
-        
+        if (transform.name == "tuer_l") {
+    
+            Debug.Log("interact!");
+
+            if (!isCarDoorOpen && doorAnim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !doorAnim.IsInTransition(0)) {
+
+                doorAnim.Play("OpenCarDoor",0,0.0f);
+                isCarDoorOpen = true;
+            }
+
+            else if (isCarDoorOpen && doorAnim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !doorAnim.IsInTransition(0)) {
+            doorAnim.Play("CloseCarDoor",0,0.0f);
+                isCarDoorOpen = false;
+
+            }
+        }
+
+        else if (transform.name == "kofferraum") {
+    
+            Debug.Log("interact!");
+
+            if (!isTrunkDoorOpen && doorAnim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !doorAnim.IsInTransition(0)) {
+
+                doorAnim.Play("OpenCarTrunk",0,0.0f);
+                isTrunkDoorOpen = true;
+            }
+
+            else if (isTrunkDoorOpen && doorAnim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !doorAnim.IsInTransition(0)) {
+            doorAnim.Play("CloseCarTrunk",0,0.0f);
+                isTrunkDoorOpen = false;
+
+            }
+        }
     }
 }
