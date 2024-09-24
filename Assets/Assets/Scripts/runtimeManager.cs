59f8067890a2b89410a566a228e6a577692a4945
Nils <nilshendrik13@gmail.com>
2024-09-24 16:08:03 +0200

    sound stops when paused

--- Assets/Assets/Scripts/runtimeManager.cs
+++ Assets/Assets/Scripts/runtimeManager.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class screenManager : MonoBehaviour
+public class runtimeManager : MonoBehaviour
 
 {
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        AudioListener.pause = false;
     }
 
     void Update()
@@ -17,11 +18,13 @@ public class screenManager : MonoBehaviour
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
+            AudioListener.pause = true;
         }
         else
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
+            AudioListener.pause = false;
         }
     }
 }
