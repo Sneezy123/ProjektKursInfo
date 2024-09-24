24464be4ab010e75059abe3c53d9f7b9fc59cad6
Nils <nilshendrik13@gmail.com>
2024-09-24 17:39:55 +0200

    key can now be picked up

--- Assets/Assets/Scripts/schlüssel1.cs
+++ Assets/Assets/Scripts/schlüssel1.cs
@@ -6,21 +6,27 @@ public class schlüssel_1 : MonoBehaviour, IInteractable
 {
 
     public scr_PlayerMovement playerScript;
+    private itemPickupManager itemPickupManager;
+
     // Start is called before the first frame update
     public int schlüsselnummer;
     void Start()
     {
-
+        itemPickupManager = GameObject.Find("ItemHolder").GetComponent<itemPickupManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            itemPickupManager.DropItem(this.gameObject);
+        }
     }
     public void Interact()
     {
         playerScript.currentItem = schlüsselnummer;
+        itemPickupManager.PickupItem(this.gameObject);
 
     }
 }
