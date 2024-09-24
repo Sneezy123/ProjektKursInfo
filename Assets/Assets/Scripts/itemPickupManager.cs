7d8bda5871114b95a8fee259051f4ffbdee41e3e
Nils <nilshendrik13@gmail.com>
2024-09-24 16:36:47 +0200

    dropping done

---
+++ Assets/Assets/Scripts/itemPickupManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemPickupManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isHolding = false;
+
+
+    void Start()
+    {
+    }
+
+    public void PickupItem(GameObject item)
+    {
+        item.transform.SetParent(this.transform);
+        item.GetComponent<Rigidbody>().isKinematic = true;
+        item.GetComponent<MeshCollider>().enabled = false;
+        item.transform.localPosition = Vector3.zero;
+        item.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+
+        isHolding = true;
+    }
+
+    public void DropItem(GameObject item)
+    {
+        item.transform.parent.DetachChildren();
+        item.GetComponent<Rigidbody>().isKinematic = false;
+        item.GetComponent<MeshCollider>().enabled = true;
+        // item.transform.localPosition = Vector3.zero;
+        // item.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+
+        isHolding = false;
+    }
+}
